9d59414f9eeb2444e4a1eddc59b43b90ed736719
Braian Melo Silva <50549436+BraianMelo@users.noreply.github.com>
2024-04-08 03:13:03 -0300

    Update materia.py

--- src/escola/materia.py
+++ src/escola/materia.py
@@ -3,12 +3,12 @@ class Materia():
     def __init__(self, nome, professor):
         self.nome = nome
         self.professor = professor
-        self.provas = []
-        self.trabalhos = []
+        self.provas = {}
+        self.trabalhos = {}
 
-    def adiconar_prova(self, prova):
-        self.provas.append(prova)
+    def adiconar_prova(self, prova, valor):
+        self.provas[prova] = valor
     
-    def adicionar_trabalho(self, trabalho):
-        self.trabalhos.append(trabalho)
+    def adicionar_trabalho(self, trabalho, valor):
+        self.trabalhos[trabalho] = valor
         
