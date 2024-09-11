834d236ece267b1b3894e2e2b2ffb649970eb227
Gabriel-Souza18 <gabrielsisou@gmail.com>
2024-09-11 10:40:36 -0300

    removendo classe Escola.py do modelo

--- Main.py
+++ Main.py
@@ -3,16 +3,13 @@ from persistencia.TurmaDAO import TurmaDAO
 from persistencia.AlunoDAO import AlunoDAO  # Supondo que exista um AlunoDAO
 from persistencia.MateriaDAO import MateriaDAO  # Supondo que exista um MateriaDAO
 from persistencia.ProfessorDAO import ProfessorDAO
-from modelo.escola.Escola import Escola
 
 
 def main():
-    # Inicializa a escola
-    escola = Escola("Escola Estadual")  
-
     # Inicializa o controlador de login
     controlador_login = ControladorLogin()
     controlador_login.iniciar_tela_login()
+
 
 if __name__ == "__main__":
     main()
--- modelo/escola/Escola.py
+++
@@ -1,6 +0,0 @@
-class Escola:
-    def __init__(self, nome):
-        self.nome = nome
-
-    def __str__(self) -> str:
-        print(self.nome)
